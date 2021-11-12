95b5e9d9e597096a0990e2e850e9002b4fee1115
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-12 12:38:56 -0600

    Add validations for create user endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,15 @@ const { response } = require('express');
 const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response) => {
-  const { email, name, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
 
+  const { email, name, password } = req.body;
   return res.json({
     ok: true,
     msg: 'Create user',
