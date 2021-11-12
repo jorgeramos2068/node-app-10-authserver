c0ad7b2713bfa3a6d6d82ebb49ed720e45ee5824
Jorge Ramos <jose.ramos@nextiva.com>
2021-11-12 16:36:37 -0600

    Create Mongoose schema for users

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model('User', UserSchema);
