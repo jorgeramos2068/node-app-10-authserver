9789fbca34f4a06dfd0d9cf2ba88f721184812e8
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-25 19:51:21 -0600

    Create custom middleware to validate fields

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { createUser, login, renew } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validateFields');
 
 const router = Router();
 
@@ -14,6 +15,7 @@ router.post(
       'password',
       'Password is required and must be longer than 5 characters'
     ).isLength({ min: 5 }),
+    validateFields,
   ],
   createUser
 );
@@ -27,6 +29,7 @@ router.post(
       'password',
       'Password is required and must be longer than 5 characters'
     ).isLength({ min: 5 }),
+    validateFields,
   ],
   login
 );
